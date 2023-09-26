10cb8e3e34ccd924875e7b6203de83baaf1c1518
Dennis Mburu <mburu.dennis@outlook.com>
2023-09-26 11:13:38 +0300

    first iteration

---
+++ index.js
@@ -0,0 +1,16 @@
+import openai from './config/open-ai.js';
+import readlineSync from 'readline-sync';
+import colors from 'colors';
+
+async function main() {
+    const chatCompletion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            { role: 'user', content: 'What is the capital of Massachusets?' }
+        ],
+    });
+
+    console.log(chatCompletion.data.choices[0].message.content);
+}
+
+main();
