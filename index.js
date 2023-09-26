8a8807064b2c58693f6470d0fced9835d98c7edd
Dennis Mburu <mburu.dennis@outlook.com>
2023-09-26 14:35:54 +0300

    final iteration 1

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ async function main() {
   console.log(colors.bold.green('Welcome to the Chatbot Program!'));
   console.log(colors.bold.green('You can start chatting with the bot.'));
 
+  const chatHistory = []; //storing conversation history
+
   while (true) {
     const userInput = readlineSync.question(colors.yellow('You: '));
 
     try {
+        //construct messages by iterating over the history
+        const messages = chatHistory.map(([role, content]) => ({ role, content }))
+
+        //add the latest user input
+        messages.push({ 'role': 'user', 'content': userInput });
+
         //call the API with user input
         const chatCompletion = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
-            messages: [{ 'role': 'user', 'content': userInput }]
+            messages: messages
         })
 
         //get completion text/content
@@ -25,6 +33,11 @@ async function main() {
         }
         
         console.log(colors.green('Bot: ') + completionText);
+
+        //update history with user input and assistant response
+        chatHistory.push(['user', userInput]);
+        chatHistory.push(['assistant', completionText]);
+
     } catch (error) {
         console.error(colors.red(error));
     }
