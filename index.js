26908883a4729e168d59381b2872d70b7772f7bb
Dennis Mburu <mburu.dennis@outlook.com>
2023-09-26 13:45:01 +0300

    second iteration

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ async function main() {
 
   while (true) {
     const userInput = readlineSync.question(colors.yellow('You: '));
+
+    try {
+        //call the API with user input
+        const completion = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'user', content: userInput }]
+        })
+
+        //get completion text/content
+        const completionText = completion.data.choices[0].message.content; 
+
+        if (userInput.toLowerCase() === 'exit') {
+            console.log(colors.green('Bot: ') + completionText);
+            return;
+        }
+        
+        console.log(colors.green('Bot: ') + completionText);
+    } catch (error) {
+        console.error(colors.red(error));
+    }
   }
 }
 
